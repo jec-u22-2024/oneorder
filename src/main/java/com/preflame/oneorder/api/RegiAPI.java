b8f5b7309d7035ad13935e7805eaceaa880c6a3f
splant <splakai2525@gmail.com>
2024-08-22 03:25:56 +0900

    検証用ブランチ発行と分離

--- src/main/java/com/preflame/oneorder/api/RegiAPI.java
+++ src/main/java/com/preflame/oneorder/api/RegiAPI.java
@@ -1,11 +1,5 @@
 package com.preflame.oneorder.api;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,7 +7,6 @@ import java.sql.SQLException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -23,9 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.preflame.oneorder.TempJson;
 import com.preflame.oneorder.model.REModel;
@@ -256,84 +247,6 @@ public class RegiAPI {
             return new REModel().getModel(HttpStatus.BAD_REQUEST);
         }
         
-        return new REModel().getModel();
-    }
-    
-    /**
-     * 画像アップロードの処理
-     * @param file
-     * @return
-     */
-    @PostMapping("/merchImage")
-    public ResponseEntity<Object> uploadMerch(@RequestPart("images") MultipartFile file) {
-        JSONObject json = new JSONObject();
-        HttpStatus stat = null;
-
-        // Path dst = Path.of("./src/main/resources/static/upload/img", file.getOriginalFilename());
-        Path dst = Path.of("./resource/upload/img", file.getOriginalFilename());
-        // Path dst = Path.of("./upload/img", file.getOriginalFilename());
-        try {
-            // byte[] fileByte = file.getBytes();
-            // Files.write(dst, fileByte);
-            Files.copy(file.getInputStream(), dst);
-            stat = HttpStatus.CREATED;
-        } catch(FileAlreadyExistsException e) {
-            try {
-                Files.delete(dst);
-                // byte[] fileByte = file.getBytes();
-                // Files.write(dst, fileByte);
-                Files.copy(file.getInputStream(), dst);
-            } catch(IOException ex) {
-                System.err.println("I/O例外");
-            }
-            stat = HttpStatus.ACCEPTED;
-        } catch(Exception e) {
-            e.printStackTrace();
-            stat = HttpStatus.BAD_REQUEST;
-            json.put("error", "Any Error");
-        }
-        ResponseEntity<Object> res = new ResponseEntity<Object>(json.toMap(), stat);
-        return res;
-    }
-
-    @PutMapping("/merchImage")
-    public ResponseEntity<Object> updateImage(@RequestPart("images") MultipartFile file, @RequestPart("oldImage") String oldPath) {
-        // Path dst = Path.of("./src/main/resources/static/upload/img", file.getOriginalFilename());
-        Path dst = Path.of("./upload/img", file.getOriginalFilename());
-        try {
-
-            // 一応古い画像を削除する処理
-            if(!oldPath.equals("assets/img/noimage.png")) {
-                // Path old = Path.of("./src/main/resources/static", oldPath);
-                Path old = Path.of(".", oldPath);
-                File oldImg = new File(old.toString());
-                if(oldImg.exists()) {
-                    oldImg.delete();
-                }
-            }
-
-
-            // byte[] fileByte = file.getBytes();
-            // Files.write(dst, fileByte);
-            Files.copy(file.getInputStream(), dst);
-            
-        } catch(FileAlreadyExistsException e) {
-            try {
-                Files.delete(dst);
-                // byte[] fileByte = file.getBytes();
-                // Files.write(dst, fileByte);
-                Files.copy(file.getInputStream(), dst);
-            } catch(IOException ex) {
-                System.err.println("I/O例外");
-                return new REModel().getModel(HttpStatus.BAD_REQUEST);
-            }
-        } catch(NoSuchFileException e) {
-            System.err.println("no file error");
-        } catch(Exception e) {
-            System.err.println("any error");
-            return new REModel().getModel(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
         return new REModel().getModel();
     }
     
---
+++ src/main/java/com/preflame/oneorder/api/UploadImage.java
@@ -0,0 +1,103 @@
+package com.preflame.oneorder.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.preflame.oneorder.model.REModel;
+
+/**
+ * 今の所jarにしたら使えない機能
+ * 
+ * 検証をしまくるために分離
+ */
+public class UploadImage {
+    /**
+     * 画像アップロードの処理
+     * @param file
+     * @return
+     */
+    @PostMapping("/merchImage")
+    public ResponseEntity<Object> uploadMerch(@RequestPart("images") MultipartFile file) {
+        JSONObject json = new JSONObject();
+        HttpStatus stat = null;
+
+        // Path dst = Path.of("./src/main/resources/static/upload/img", file.getOriginalFilename());
+        Path dst = Path.of("./resource/upload/img", file.getOriginalFilename());
+        // Path dst = Path.of("./upload/img", file.getOriginalFilename());
+        try {
+            // byte[] fileByte = file.getBytes();
+            // Files.write(dst, fileByte);
+            Files.copy(file.getInputStream(), dst);
+            stat = HttpStatus.CREATED;
+        } catch(FileAlreadyExistsException e) {
+            try {
+                Files.delete(dst);
+                // byte[] fileByte = file.getBytes();
+                // Files.write(dst, fileByte);
+                Files.copy(file.getInputStream(), dst);
+            } catch(IOException ex) {
+                System.err.println("I/O例外");
+            }
+            stat = HttpStatus.ACCEPTED;
+        } catch(Exception e) {
+            e.printStackTrace();
+            stat = HttpStatus.BAD_REQUEST;
+            json.put("error", "Any Error");
+        }
+        ResponseEntity<Object> res = new ResponseEntity<Object>(json.toMap(), stat);
+        return res;
+    }
+
+    @PutMapping("/merchImage")
+    public ResponseEntity<Object> updateImage(@RequestPart("images") MultipartFile file, @RequestPart("oldImage") String oldPath) {
+        // Path dst = Path.of("./src/main/resources/static/upload/img", file.getOriginalFilename());
+        Path dst = Path.of("./upload/img", file.getOriginalFilename());
+        try {
+
+            // 一応古い画像を削除する処理
+            if(!oldPath.equals("assets/img/noimage.png")) {
+                // Path old = Path.of("./src/main/resources/static", oldPath);
+                Path old = Path.of(".", oldPath);
+                File oldImg = new File(old.toString());
+                if(oldImg.exists()) {
+                    oldImg.delete();
+                }
+            }
+
+
+            // byte[] fileByte = file.getBytes();
+            // Files.write(dst, fileByte);
+            Files.copy(file.getInputStream(), dst);
+            
+        } catch(FileAlreadyExistsException e) {
+            try {
+                Files.delete(dst);
+                // byte[] fileByte = file.getBytes();
+                // Files.write(dst, fileByte);
+                Files.copy(file.getInputStream(), dst);
+            } catch(IOException ex) {
+                System.err.println("I/O例外");
+                return new REModel().getModel(HttpStatus.BAD_REQUEST);
+            }
+        } catch(NoSuchFileException e) {
+            System.err.println("no file error");
+        } catch(Exception e) {
+            System.err.println("any error");
+            return new REModel().getModel(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new REModel().getModel();
+    }
+}
